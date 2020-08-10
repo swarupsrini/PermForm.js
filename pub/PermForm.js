3e33c7156bbbfe6facd0533df57de60321d3d6f9
Swarup Srini <gswarup2001@gmail.com>
2020-08-10 03:16:46 -0400

    Finished dropDown for basic theme, also redid positioning system to be composition-based (since TA mentioned it was difficult to use) and adapted localStorage save/load to new state system

--- pub/PermForm.js
+++ pub/PermForm.js
@@ -1,8 +1,6 @@
 /* PermForm.js Library */
 "use strict";
 
-// const e = require("express");
-
 const log = console.log;
 log("PermForm.js");
 
@@ -17,21 +15,24 @@ log("PermForm.js");
 function initForm(specs) {
   const _self = {};
   _self.specs = specs;
+  // localStorage.clear(); // TEMP
   if (
     specs.autoSave &&
-    localStorage.hasOwnProperty("PermForm-" + _self.divId)
+    localStorage.hasOwnProperty("PermForm-" + specs.divId)
   ) {
-    _self.state = localStorage.getItem("PermForm-" + _self.divId).split(",");
-    _self.state = !Array.isArray(_self.state)
-      ? new Array(specs.elements.length).fill(null)
-      : _self.state;
+    _self.state = localStorage.getItem("PermForm-" + specs.divId).split(",");
+    _self.state = JSON.parse(_self.state);
+    _self.state = !Array.isArray(_self.state) ? [] : _self.state;
   } else {
-    _self.state = new Array(specs.elements.length).fill(null);
+    _self.state = [];
   }
 
   window.addEventListener("beforeunload", (e) => {
     specs.autoSave &&
-      localStorage.setItem("PermForm-" + _self.divId, _self.state);
+      localStorage.setItem(
+        "PermForm-" + specs.divId,
+        JSON.stringify(_self.state)
+      );
   });
 
   /**
@@ -42,46 +43,41 @@ function initForm(specs) {
    */
   _self.buildForm = function () {
     const specs = _self.specs;
-    const theme = formInfo.themes[specs.theme];
+    const theme = _self.__formInfo.themes[specs.theme];
     const formStyle = theme.formStyle;
     const elements = theme.elements;
 
     const divElem = document.getElementById(specs.divId);
     const formElem = document.createElement("form");
 
-    log(typeof formStyle);
-
     Object.keys(formStyle).forEach(
       (item) => (formElem.style[item] = formStyle[item])
     );
 
-    formElem.style.gridTemplateColumns = "minmax(0, 1fr) ".repeat(
-      specs.columns
-    );
+    formElem.style.gridTemplateColumns = "1fr ".repeat(specs.columns);
+    formElem.style.gridTemplateRows = "1fr ".repeat(specs.elements.length);
 
-    specs.elements.forEach((elemSpecs, i) => {
-      const setState = (s) => {
-        _self.state[i] = s;
-      };
-      const height = elemSpecs.size.split(",")[0];
-      if (elemSpecs.type in elements) {
-        log(elements.dropDown);
-        const elem = elements[elemSpecs.type](
-          elemSpecs,
-          _self.state[i],
-          setState,
-          height
-        );
-        elem.style.gridColumn =
-          elemSpecs.position.split(",")[1] +
-          " / span " +
-          elemSpecs.size.split(",")[1];
-        elem.style.gridRow =
-          elemSpecs.position.split(",")[0] +
-          " / span " +
-          elemSpecs.size.split(",")[0];
-        formElem.appendChild(elem);
-      }
+    let extra = 0;
+    specs.elements.forEach((row, x) => {
+      row.forEach((elemSpecs, y) => {
+        const setState = (s) => {
+          _self.state[x][y] = s;
+        };
+        if (!_self.state[x]) _self.state[x] = [];
+        if (elemSpecs.type in elements) {
+          const size = elemSpecs.size.split(",");
+          const elem = elements[elemSpecs.type](
+            elemSpecs,
+            _self.state[x][y],
+            setState,
+            size[1]
+          );
+          elem.style.gridColumn = y + 1 + " / span " + size[1];
+          elem.style.gridRow = x + extra + 1 + "/ span " + size[0];
+          if (size[0] > 1) extra += parseInt(size[0]) - 1;
+          formElem.appendChild(elem);
+        }
+      });
     });
 
     formElem.addEventListener("submit", (e) => {
@@ -106,147 +102,176 @@ function initForm(specs) {
     return _self.state;
   };
 
-  return _self;
-}
-
-/**
- * The mapping that stores theme information, along with the code for each
- * component part of that theme. Also stores validator functions for each
- * element.
- *
- * Currently implemented themes: basic
- * Currently implemented elements: label, text input, file input, submit
- *
- * Future elements: drop down, check box, radio, slider, date picker
- */
-const formInfo = {
-  themes: {
-    basic: {
-      formStyle: {
-        fontSize: "20px",
-        fontFamily: "Arial",
-        gridColumnGap: "10px",
-        gridRowGap: "20px",
-        display: "grid",
-        padding: "10px",
-      },
-      elements: {
-        textLabel: (elemSpecs, state, setState) => {
-          const elem = document.createElement("label");
-          elem.innerText = elemSpecs.value;
-          Object.assign(elem.style, {
-            fontSize: "1em",
-          });
-          return elem;
+  /**
+   * The mapping that stores theme information, along with the code for each
+   * component part of that theme. Also stores validator functions for each
+   * element.
+   *
+   * Currently implemented:
+   * theme: basic
+   * elements: label, text input, file input, submit, drop down
+   *
+   * Future elements: check box, radio, slider, date picker
+   */
+  _self.__formInfo = {
+    themes: {
+      basic: {
+        formStyle: {
+          fontSize: "20px",
+          fontFamily: "Roboto",
+          gridColumnGap: "10px",
+          gridRowGap: "10px",
+          display: "grid",
+          height: "100%",
+          padding: "10px",
         },
-        textInput: (elemSpecs, state, setState, height) => {
-          const elem = document.createElement("textArea");
-          elem.placeholder = elemSpecs.placeholder;
-          if (state) elem.value = state;
-          elem.addEventListener("change", (e) => {
-            setState(elem.value);
-            if (
-              elemSpecs.validator in formInfo.validators &&
-              !formInfo.validators[elemSpecs.validator](elem.value)
-            ) {
-              elem.style.border = "2px solid red";
-              elem.setCustomValidity("Invalid field.");
-            } else {
-              elem.style.border = "";
-              elem.setCustomValidity("");
-            }
-          });
-          elem.required = elemSpecs.required;
-          Object.assign(elem.style, {
-            fontFamily: "inherit",
-            resize: "none",
-            fontSize: ".75em",
-            padding: ".5em 1em",
-            height: 20 * height + "px",
-            verticalAlign: "top",
-          });
-          return elem;
-        },
-        fileInput: (elemSpecs, state, setState, height) => {
-          const div = document.createElement("div");
-          const button = document.createElement("button");
-          const input = document.createElement("input");
-          Object.assign(div.style, {
-            position: "relative",
-            overflow: "hidden",
-          });
-          button.innerHTML = elemSpecs.name;
-          if (state) button.innerHTML = elemSpecs.name + ": " + state;
-          Object.assign(button.style, {
-            fontSize: ".75em",
-            width: "100%",
-            padding: ".5em",
-          });
-          input.type = "file";
-          input.required = elemSpecs.required;
-          input.addEventListener("change", (e) => {
-            let fileName = input.value.split("\\").pop();
-            if (fileName.length == 0) fileName = "";
-            else if (fileName.length > 50)
-              fileName = fileName.substring(0, 50) + "...";
-            button.innerHTML = elemSpecs.name + ": " + fileName;
-            setState(fileName);
-          });
-          Object.assign(input.style, {
-            fontSize: "100px",
-            position: "absolute",
-            left: 0,
-            top: 0,
-            opacity: 0,
-          });
-          div.appendChild(button);
-          div.appendChild(input);
-          return div;
-        },
-        dropDown: (elemSpecs, state, changeState, height) => {
-          log("drop");
-          const select = document.createElement("select");
-          const label = document.createElement("option");
-          label.innerHtml = elemSpecs.name;
-          label.disabled = true;
-          label.selected = true;
-          select.appendChild(label);
-          elemSpecs.options.forEach((op) => {
-            const opElem = document.createElement("option");
-            opElem.innerHTML = op;
-            op.value = op;
-          });
-          Object.assign(select.style, {
-            padding: "8x 12px",
-            color: "#333333",
-            backgroundColor: "#eeeeee",
-            border: "1px solid #dddddd",
-            cursor: "pointer",
-            borderRadius: "5px",
-          });
-          log("done");
-          return select;
-        },
-        submit: (elemSpecs, state, changeState, height) => {
-          const input = document.createElement("input");
-          input.type = "submit";
-          Object.assign(input.style, {
-            backgroundColor: "#4CAF50",
-            color: "white",
-            fontSize: ".75em",
-            width: "100%",
-            padding: ".5em",
-            border: "3px solid #4CAF50",
-            borderRadius: "5px",
-            cursor: "pointer",
-          });
-          return input;
+        elements: {
+          textLabel: (elemSpecs, state, setState, height) => {
+            const elem = document.createElement("label");
+            elem.innerText = elemSpecs.value;
+            Object.assign(elem.style, {
+              fontSize: "1em",
+              verticalAlign: "bottom",
+              alignSelf: "end",
+            });
+            return elem;
+          },
+          textInput: (elemSpecs, state, setState, height) => {
+            const elem = document.createElement(
+              height == 1 ? "input" : "textArea"
+            );
+            elem.placeholder = elemSpecs.placeholder;
+            if (state) elem.value = state;
+            elem.addEventListener("input", (e) => {
+              setState(elem.value);
+              if (
+                elemSpecs.validator in _self.__formInfo.validators &&
+                !_self.__formInfo.validators[elemSpecs.validator](elem.value)
+              ) {
+                elem.style.border = "2px solid red";
+                elem.setCustomValidity("Invalid field.");
+              } else {
+                elem.style.border = "";
+                elem.style.borderWidth = "1px";
+                elem.setCustomValidity("");
+              }
+            });
+            elem.required = elemSpecs.required;
+            elem.wrap = "off";
+            Object.assign(elem.style, {
+              fontFamily: "inherit",
+              resize: "none",
+              overflow: "hidden",
+              borderRadius: "3px",
+              borderWidth: "1px",
+              fontSize: ".75em",
+              padding: ".5em 1em",
+              height: "100%",
+              boxSizing: "border-box",
+              verticalAlign: "top",
+            });
+            return elem;
+          },
+          fileInput: (elemSpecs, state, setState, height) => {
+            const div = document.createElement("div");
+            const button = document.createElement("button");
+            const input = document.createElement("input");
+            // div
+            Object.assign(div.style, {
+              position: "relative",
+              width: "100%",
+              height: "100%",
+              overflow: "hidden",
+              boxSizing: "border-box",
+            });
+            // button
+            button.innerHTML = elemSpecs.name;
+            if (state) button.innerHTML = elemSpecs.name + ": " + state;
+            Object.assign(button.style, {
+              fontSize: ".75em",
+              fontFamily: "inherit",
+              width: "100%",
+              height: "100%",
+              boxSizing: "border-box",
+              padding: ".5em",
+            });
+            // input
+            input.type = "file";
+            input.required = elemSpecs.required;
+            input.addEventListener("change", (e) => {
+              let fileName = input.value.split("\\").pop();
+              if (fileName.length == 0) fileName = "";
+              else if (fileName.length > 50)
+                fileName = fileName.substring(0, 50) + "...";
+              button.innerHTML = elemSpecs.name + ": " + fileName;
+              setState(fileName);
+            });
+            Object.assign(input.style, {
+              fontSize: "100px",
+              position: "absolute",
+              left: 0,
+              top: 0,
+              opacity: 0,
+            });
+            div.appendChild(button);
+            div.appendChild(input);
+            return div;
+          },
+          dropDown: (elemSpecs, state, setState, height) => {
+            const select = document.createElement("select");
+            const label = document.createElement("option");
+            // label
+            label.innerHTML = elemSpecs.name;
+            label.disabled = true;
+            label.selected = true;
+            label.style.fontFamily = "inherit";
+            // select
+            select.appendChild(label);
+            elemSpecs.options.forEach((op) => {
+              const opElem = document.createElement("option");
+              opElem.innerHTML = op;
+              opElem.value = op;
+              if (op == state) opElem.selected = true;
+              select.appendChild(opElem);
+            });
+            select.addEventListener("change", (e) => {
+              setState(select.options[select.selectedIndex].value);
+            });
+            Object.assign(select.style, {
+              padding: "8x 12px",
+              color: "#333333",
+              backgroundColor: "#eeeeee",
+              fontFamily: "inherit",
+              fontSize: ".75em",
+              cursor: "pointer",
+              borderRadius: "3px",
+            });
+            return select;
+          },
+          submit: (elemSpecs, state, setState, height) => {
+            const input = document.createElement("input");
+            input.type = "submit";
+            Object.assign(input.style, {
+              backgroundColor: "#4CAF50",
+              color: "white",
+              fontSize: ".75em",
+              fontFamily: "inherit",
+              width: "100%",
+              padding: ".5em",
+              border: "3px solid #4CAF50",
+              borderRadius: "5px",
+              cursor: "pointer",
+            });
+            return input;
+          },
         },
       },
     },
-  },
-  validators: {
-    numeric: (value) => !isNaN(value),
-    email: (value) => /(^\S+@\S+$)|^$/.test(value),
-  },
-};
+    validators: {
+      numeric: (value) => !isNaN(value),
+      email: (value) => /(^\S+@\S+$)|^$/.test(value),
+    },
+  };
+
+  return _self;
+}
--- pub/examples.js
+++ pub/examples.js
@@ -8,76 +8,83 @@ initForm({
   theme: "basic",
   columns: 2,
   elements: [
-    {
-      type: "textLabel",
-      value: "Welcome to the application!",
-      position: "1,1",
-      size: "1,2",
-    },
-    {
-      type: "textInput",
-      placeholder: "First Name",
-      required: true,
-      size: "1,1",
-      position: "2,1",
-    },
-    {
-      type: "textInput",
-      placeholder: "Last Name",
-      required: true,
-      size: "1,1",
-      position: "2,2",
-    },
-    {
-      type: "textInput",
-      placeholder: "Phone Number",
-      required: true,
-      position: "3,1",
-      size: "1,1",
-      validator: "numeric",
-    },
-    {
-      type: "textInput",
-      placeholder: "Email",
-      required: true,
-      position: "3,2",
-      size: "1,1",
-      validator: "email",
-    },
-    {
-      type: "textInput",
-      placeholder: "Why do you want this job?",
-      required: true,
-      position: "4,1",
-      size: "4,2",
-    },
-    {
-      type: "fileInput",
-      name: "Upload Resume",
-      required: true,
-      position: "8,1",
-      size: "1,2",
-      validator: "pdf",
-    },
-    {
-      type: "textInput",
-      placeholder: "Is your info correct?",
-      required: true,
-      position: "9,1",
-      size: "1,2",
-    },
-    {
-      type: "dropDown",
-      name: "Choose an option",
-      required: true,
-      position: "10,1",
-      size: "1,2",
-    },
-    {
-      type: "submit",
-      position: "9,1",
-      size: "1,2",
-    },
+    [
+      {
+        type: "textLabel",
+        value: "Welcome to the application!",
+        size: "1,2",
+      },
+    ],
+    [
+      {
+        type: "textInput",
+        placeholder: "First Name",
+        required: true,
+        size: "1,1",
+      },
+      {
+        type: "textInput",
+        placeholder: "Last Name",
+        required: true,
+        size: "1,1",
+      },
+    ],
+    [
+      {
+        type: "textInput",
+        placeholder: "Phone Number",
+        required: true,
+        size: "1,1",
+        validator: "numeric",
+      },
+      {
+        type: "textInput",
+        placeholder: "Email",
+        required: true,
+        size: "1,1",
+        validator: "email",
+      },
+    ],
+    [
+      {
+        type: "textInput",
+        placeholder: "Why do you want this job?",
+        required: true,
+        size: "2,2",
+      },
+    ],
+    [
+      {
+        type: "fileInput",
+        name: "Upload Resume",
+        required: true,
+        size: "1,2",
+        validator: "pdf",
+      },
+    ],
+    [
+      {
+        type: "textLabel",
+        value: "Are you sure your info is correct?",
+        required: true,
+        size: "1,2",
+      },
+    ],
+    [
+      {
+        type: "dropDown",
+        name: "Choose an option",
+        options: ["Yes", "No"],
+        required: true,
+        size: "1,2",
+      },
+    ],
+    [
+      {
+        type: "submit",
+        size: "1,2",
+      },
+    ],
   ],
   onSubmit: (state) => {
     console.log(state);
